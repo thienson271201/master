5166d81ddda47c1acc30e777a808d5e876ac5f9f
HoangMinh02 <117251807+HoangMinh02@users.noreply.github.com>
2025-05-09 21:33:26 +0700

    404

---
+++ giaodien/404.php
@@ -0,0 +1,28 @@
+   <section class="text-center content-section">
+      <div class="container">
+        <img
+          class="image offs-md"
+          src="assets/images/error/404.png"
+          alt=""
+          data-inview-showup="showup-scale"
+        />
+        <div class="section-head text-center container-md">
+          <h2
+            class="section-title text-upper text-lg"
+            data-inview-showup="showup-translate-right"
+          >
+            Lỗi
+          </h2>
+          <p data-inview-showup="showup-translate-left">
+            Trang này không được tìm thấy
+          </p>
+        </div>
+        <a
+          class="btn text-upper"
+          href="index.php?page=trangChu"
+          data-inview-showup="showup-translate-up"
+          >Quay về trang chủ</a
+        >
+      </div>
+    </section>
+    
