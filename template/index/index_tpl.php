61d142bfd4ba0f6f6e72aebd8d46e931c859d174
HoangMinh02 <117251807+HoangMinh02@users.noreply.github.com>
2025-05-21 20:57:56 +0700

    đổi tên 1 section_loi_ich

--- template/index/index_tpl.php
+++ template/index/index_tpl.php
@@ -7,7 +7,7 @@
 
 require_once 'components/banner.php';
 require_once 'components/category.php';
-require_once 'components/conten_section_01.php';
+require_once 'components/section_loi_ich.php';
 require_once 'components/sanphamnoibat.php';
 require_once 'components/container.php';
 require_once 'components/conten_section_23.php';
