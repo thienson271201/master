ac17327638514a2725cb79afc465adc9713427eb
HoangMinh02 <117251807+HoangMinh02@users.noreply.github.com>
2025-05-24 00:00:29 +0700

    up lại css

--- template/layout/css.php
+++ template/layout/css.php
@@ -9,4 +9,7 @@
 <link href="assets/pentix/css/pentix.min.css" rel="stylesheet" type="text/css" />
 <link href="assets/css/pex-theme.min.css" rel="stylesheet" type="text/css" />
 
-<link rel="stylesheet" href="assets/css/style.css?v=<?= rand(10, 99) ?>">
+<!-- <link rel="stylesheet" href="assets/css/style.css?v=<?= rand(10, 99) ?>"> -->
+<link rel="stylesheet" href="assets/css/style.css">
+
+<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
--- template/layout/js.php
+++ template/layout/js.php
@@ -11,4 +11,3 @@
 <script async defer="defer"
   src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBDBAbNXaCDOzujLCykXUvTylfbL1wUcaM&amp;callback=initMap"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
