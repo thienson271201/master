880e2bd9c86cf88d2c0d18a6992ccbe0a636fecb
thienson271201 <huynhthienson01012002@gmail.com>
2025-07-16 23:47:09 +0700

    them banner

--- template/index/components/banner.php
+++ template/index/components/banner.php
@@ -39,65 +39,21 @@
             <div class="slide">
               <div class="banner-simple">
                 <img
-                  src="assets/images/shop/back-image-3.jpg"
+                  src="assets/images/shop/banner01.jpg"
                   alt=""
                   data-cover-image="true"
                 />
-                <div class="banner-row p-2 p-sm-4 p-md-5">
-                  <div class="banner-row-image mb-0 sm-col-7 mb-sm-auto">
-                    <img src="assets/images/shop/notebook.png" alt="" />
-                  </div>
-                  <div
-                    class="banner-row-textes mt-4 sm-col-5 sm-text-left mt-sm-auto"
-                  >
-                    <div class="banner-title res-text-xl text-colorful">
-                      Chuẩn Mới - Laptop Đẳng Cấp
-                    </div>
-                    <div class="banner-text res-text-sm">
-                      Màn hình lớn, thiết kế siêu mỏng, sức mạnh vượt trội
-                    </div>
-                    <div class="banner-price-text res-text-xs mt-3 mt-md-4">
-                      Giá chỉ từ
-                    </div>
-                    <div class="banner-price-value res-text-lg alt-color">
-                      12.990.000đ
-                    </div>
-                    <a class="banner-btn mt-3 btn" href="#">Mua ngay</a>
-                  </div>
-                </div>
+                
               </div>
             </div>
             <div class="slide">
               <div class="banner-simple">
                 <img
-                  src="assets/images/shop/back-image-2.jpg"
+                  src="assets/images/shop/banner02.jpg"
                   alt=""
                   data-cover-image="true"
                 />
-                <div class="banner-row p-2 p-sm-4 p-md-5">
-                  <div
-                    class="banner-row-image mb-0 sm-col-5 order-sm-last mb-sm-auto"
-                  >
-                    <img src="assets/images/shop/notebook.png" alt="" />
-                  </div>
-                  <div
-                    class="banner-row-textes mt-4 sm-col-7 sm-text-left mt-sm-auto"
-                  >
-                    <div class="banner-title res-text-xl text-colorful">
-                      Chuẩn Mới - Laptop Đẳng Cấp
-                    </div>
-                    <div class="banner-text res-text-sm">
-                      Màn hình lớn, thiết kế siêu mỏng, sức mạnh vượt trội
-                    </div>
-                    <div class="banner-price-text res-text-xs mt-3 mt-md-4">
-                      Giá chỉ từ
-                    </div>
-                    <div class="banner-price-value res-text-lg alt-color">
-                      12.990.000đ
-                    </div>
-                    <a class="banner-btn mt-3 btn" href="#">Mua ngay</a>
-                  </div>
-                </div>
+                
               </div>
             </div>
           </div>
